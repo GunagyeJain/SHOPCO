50e658007e39dcb34feca628050949d93f27b739
Gunagye Jain <gunagye.jain@gmail.com>
2026-06-18 11:14:33 +0530

    react router setup with home, category, product-detail and cart pages

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router";
+import Home from "../pages/Home";
+import ProductDetail from "../pages/ProductDetail";
+import Category from "../pages/Category";
+import Cart from "../pages/Cart";
+
+const AppRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product-detail" element={<ProductDetail />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/cart" element={<Cart />} />
+        {/* <Route element={<AuthLayout />}>
+
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route> */}
+      </Routes>
+    </div>
+  );
+};
+
+export default AppRoutes;
