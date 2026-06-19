d2e2ffff3129a1a2ca3201ff3c6962d92f84c248
Gunagye Jain <gunagye.jain@gmail.com>
2026-06-19 12:16:53 +0530

    updated app.jsx with redux addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useDispatch } from "react-redux";
 import AppRoutes from "./routes/AppRoutes";
+import { useEffect } from "react";
+
+import { restoreSession, setUser } from "./features/auth/authSlice";
+import { supabase } from "./lib/supabaseClient";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(restoreSession());
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      dispatch(setUser(session?.user ?? null));
+    });
+
+    return () => subscription.unsubscribe();
+  }, [dispatch]);
+
   return <AppRoutes />;
 };
 
